3010e09f75ffbec316682931b1b63435fdfd4e2b
2jumper3 <2jumper3@gmail.com>
2019-10-21 11:26:19 +0300

    Final

---
+++ AviaScan/CoreData/CoreDataService.h
@@ -0,0 +1,17 @@
+//
+//  CoreDataService.h
+//  AviaScan
+//
+//  Created by Sergey on 07/10/2019.
+//  Copyright © 2019 Sergey. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CoreDataService : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AviaScan/CoreData/PictureStruct.m
@@ -0,0 +1,13 @@
+//
+//  PictureStruct.m
+//  AviaScan
+//
+//  Created by Sergey on 15.10.2019.
+//  Copyright © 2019 Sergey. All rights reserved.
+//
+
+#import "PictureStruct.h"
+
+@implementation PictureStruct
+
+@end
